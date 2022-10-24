f016ec0f7b5671265ae3ef3f8a3e13f7a492bae2
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-24 11:04:37 +0600

    server side setup and error handler added

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const port = process.env.PORT || 4000;
 // middleware
 app.use(express.json());
 
+// default error handling
+const errorHandler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500).json({ error: err });
+};
+
 // listening to the poert
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
