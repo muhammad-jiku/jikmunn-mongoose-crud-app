0ad53e00aa5590b21a4198a77b3cdc9cadba4d3b
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-27 12:17:44 +0600

    dotenv and jwt setup successful

--- routes/routeHandler/userHandler.js
+++ routes/routeHandler/userHandler.js
@@ -1,12 +1,14 @@
 // dependencies
+require('dotenv').config(); // also a middleware
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const router = express.Router();
 const userSchema = require('../../models/schemas/userSchema');
 const User = new mongoose.model('User', userSchema);
 
-// Signing  up
+// Signing up
 // using callback function
 router.post('/sign-up', async (req, res) => {
   try {
@@ -18,6 +20,48 @@ router.post('/sign-up', async (req, res) => {
     });
     await newUser.save();
     res.status(200).json({ message: 'Signing up new user successfully!!' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'There is a server side error!' });
+  }
+});
+
+// Signing in
+// using callback function
+router.post('/sign-in', async (req, res) => {
+  try {
+    const user = User.find({ username: req.body.username });
+    if (user && user.length) {
+      const isValidPassword = await bcrypt.compare(
+        req.body.password,
+        user[0].password
+      );
+      if (isValidPassword) {
+        // generate token
+        const token = jwt.sign(
+          {
+            username: user[0].username,
+            userId: user[0]._id,
+          },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: '1d',
+          }
+        );
+
+        // sending response
+        res.status(200).json({
+          accessToken: token,
+          message: 'Sign-in successful!!',
+        });
+      } else {
+        console.log(err);
+        res.status(401).json({ error: 'Authentication failed!' });
+      }
+    } else {
+      console.log(err);
+      res.status(401).json({ error: 'Authentication failed!' });
+    }
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: 'There is a server side error!' });
